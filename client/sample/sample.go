290c9d4339ef4260d089c64ed87ef9c0982572c3
Yuriy Kovalchuk <failwin@ukr.net>
2023-12-15 23:57:10 +0200

    client 0.0.7

--- client/sample/sample.go
+++ client/sample/sample.go
@@ -12,6 +12,6 @@ func Hello(name string) string {
 	utils.Utils_test()
 
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome v0.0.7!", name)
 	return message
 }
